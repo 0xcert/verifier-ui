93af3b8b16f1e16911a851b390e73885f5cd5b68
davidlicen <david@artnetik.si>
2019-10-14 16:08:55 +0200

    Imlement results

---
+++ components/Results.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="body">
+    <div class="flex">
+      <div class="flex-grow">
+        <p class="max-width">
+          Proactively harness sticky deliverables via bleeding-edge bandwidth. Distinctively e-enable error-free mindshare for cross-media materials. Assertively plagiarize clicks-and-mortar outsourcing before interdependent meta-services. Continually facilitate empowered potentialities after accurate convergence. Globally transform parallel.
+        </p>
+      </div>
+      <img src="/images/trusted.svg" alt="Trusted">
+    </div>
+    <pre>
+    {{ data }}
+    </pre>
+    <table class="table">
+      <tr>
+        <td>Ledger name:</td>
+        <td>ISO-9001-2019</td>
+      </tr>
+      <tr>
+        <td>Ledger symbol:</td>
+        <td>SIQ</td>
+      </tr>
+      <tr>
+        <td>Ledger address:</td>
+        <td>0x4234928123740328428741</td>
+      </tr>
+      <tr>
+        <td>Key:</td>
+        <td>first_name</td>
+      </tr>
+      <tr>
+        <td>Value:</td>
+        <td>David</td>
+      </tr>
+      <tr>
+        <td>Status:</td>
+        <td><status valid /></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+import Status from '~/components/Status'
+
+export default {
+  components: {
+    Status
+  },
+  props: {
+    data: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
--- pages/verify.vue
+++ pages/verify.vue
@@ -2,101 +2,112 @@
   <div class="container">
     <div class="main box">
       <Header />
-      <div class="body">
-        <h1>Verify your asset</h1>
-        <p>Fill the fields and validate an asset online, completely decentralized. </p>
-        <ValidationObserver v-slot="{ passes }">
-          <form class="form mt-1" @submit.prevent="passes(verifyAsset)">
-            <validation-provider
-              v-slot="{ errors }"
-              rules="required"
-              name="DID"
-              :class="'form-group'"
-            >
-              <input
-                v-model="data.did"
-                type="text"
-                class="form-field"
-                placeholder="Enter a valid DID."
+      <transition name="fade" mode="out-in">
+        <div v-if="!submitted" key="input" class="body">
+          <h1>Verify your asset</h1>
+          <p>Fill the fields and validate an asset online, completely decentralized. </p>
+          <ValidationObserver v-slot="{ passes }">
+            <form class="form mt-1" @submit.prevent="passes(verifyAsset)">
+              <validation-provider
+                v-slot="{ errors }"
+                rules="required"
+                name="DID"
+                :class="'form-group'"
               >
-              <label for="email" v-text="'Enter a valid DID'" />
-              <div class="alert" v-text="errors[0]" />
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              rules="required|json"
-              name="Schema"
-              :class="'form-group'"
-            >
-              <resizable-textarea>
-                <textarea
-                  v-model="data.schema"
+                <input
+                  v-model="data.did"
                   type="text"
-                  rows="1"
-                  class="form-field code"
-                  placeholder="Enter a valid asset schema."
-                />
-              </resizable-textarea>
-              <label for="schema" v-text="'Enter a valid asset schema.'" />
-              <div class="alert" v-text="errors[0]" />
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              rules="required|json"
-              name="Evidence"
-              :class="'form-group'"
-            >
-              <resizable-textarea>
-                <textarea
-                  v-model="data.evidence"
-                  type="text"
-                  rows="1"
-                  class="form-field code"
-                  placeholder="Enter a valid asset evidence"
-                />
-              </resizable-textarea>
-              <label for="evidence" v-text="'Enter a valid asset evidence'" />
-              <div class="alert" v-text="errors[0]" />
-            </validation-provider>
-            <validation-provider
-              v-slot="{ errors }"
-              rules="required|json"
-              name="Metadata"
-              :class="'form-group'"
-            >
-              <resizable-textarea>
-                <textarea
-                  v-model="data.metadata"
-                  type="text"
-                  rows="1"
-                  class="form-field code"
-                  placeholder="Enter asset Metadata"
-                />
-              </resizable-textarea>
-              <label for="metadata" v-text="'Enter asset Metadata'" />
-              <div class="alert" v-text="errors[0]" />
-            </validation-provider>
-            <div class="form-group">
-              <select v-model="network" name="network" class="form-field select">
-                <option :value="1">
-                  Mainnet
-                </option>
-                <option :value="2">
-                  Rinkeby
-                </option>
-                <option :value="3">
-                  Ropsten
-                </option>
-              </select>
-              <label for="metadata" v-text="'Select network'" />
-            </div>
-            <button type="submit" class="button mt-2">
-              Verify Asset
-            </button>
-          </form>
-        </ValidationObserver>
-      </div>
-      <Notice />
+                  class="form-field"
+                  placeholder="Enter a valid DID."
+                >
+                <label for="email" v-text="'Enter a valid DID'" />
+                <div class="alert" v-text="errors[0]" />
+              </validation-provider>
+              <validation-provider
+                v-slot="{ errors }"
+                rules="required|json"
+                name="Schema"
+                :class="'form-group'"
+              >
+                <resizable-textarea>
+                  <textarea
+                    v-model="data.schema"
+                    type="text"
+                    rows="1"
+                    class="form-field code"
+                    placeholder="Enter a valid asset schema."
+                  />
+                </resizable-textarea>
+                <label for="schema" v-text="'Enter a valid asset schema.'" />
+                <div class="alert" v-text="errors[0]" />
+              </validation-provider>
+              <validation-provider
+                v-slot="{ errors }"
+                rules="required|json"
+                name="Evidence"
+                :class="'form-group'"
+              >
+                <resizable-textarea>
+                  <textarea
+                    v-model="data.evidence"
+                    type="text"
+                    rows="1"
+                    class="form-field code"
+                    placeholder="Enter a valid asset evidence"
+                  />
+                </resizable-textarea>
+                <label for="evidence" v-text="'Enter a valid asset evidence'" />
+                <div class="alert" v-text="errors[0]" />
+              </validation-provider>
+              <validation-provider
+                v-slot="{ errors }"
+                rules="required|json"
+                name="Metadata"
+                :class="'form-group'"
+              >
+                <resizable-textarea>
+                  <textarea
+                    v-model="data.metadata"
+                    type="text"
+                    rows="1"
+                    class="form-field code"
+                    placeholder="Enter asset Metadata"
+                  />
+                </resizable-textarea>
+                <label for="metadata" v-text="'Enter asset Metadata'" />
+                <div class="alert" v-text="errors[0]" />
+              </validation-provider>
+              <div class="form-group">
+                <select v-model="network" name="network" class="form-field select">
+                  <option :value="1">
+                    Mainnet
+                  </option>
+                  <option :value="2">
+                    Rinkeby
+                  </option>
+                  <option :value="3">
+                    Ropsten
+                  </option>
+                </select>
+                <label for="metadata" v-text="'Select network'" />
+              </div>
+              <button type="submit" class="button mt-2">
+                Verify Asset
+              </button>
+            </form>
+          </ValidationObserver>
+        </div>
+        <div v-if="loading" key="loading" class="loading">
+          Loading ...
+        </div>
+        <div v-else key="results" class="results">
+          <results :data="data" />
+          <button class="button" @click="resetForm()">
+            Verify Another Asset
+          </button>
+        </div>
+      </transition>
+      <notice />
     </div>
     <Footer />
   </div>
@@ -110,6 +121,7 @@ import Footer from '~/components/Footer'
 import Header from '~/components/Header'
 import Notice from '~/components/Notice'
 import ResizableTextarea from '~/components/ResizableTextarea'
+import Results from '~/components/Results'
 
 extend('required', required)
 extend('email', email)
@@ -131,6 +143,7 @@ export default {
     Notice,
     Header,
     Footer,
+    Results,
     ResizableTextarea,
     ValidationProvider,
     ValidationObserver
@@ -138,12 +151,21 @@ export default {
   data () {
     return {
       data: {},
-      network: 1
+      network: 1,
+      submitted: false,
+      loading: false
     }
   },
   methods: {
     verifyAsset () {
-      this.$router.push('/isvalid/data')
+      this.loading = true
+      this.submitted = true
+      this.loading = false
+    },
+    resetForm () {
+      this.submitted = false
+      this.loading = false
+      this.data = {}
     }
   }
 }
@@ -151,4 +173,11 @@ export default {
 
 <style lang="scss">
 @import '~assets/_forms.scss';
+
+.loading {
+  min-height: 462px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
