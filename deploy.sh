afefa301d7c4a8f192cfa3bb53df5a7b05d25c92
davidlicen <david@artnetik.si>
2019-10-16 15:32:46 +0200

    Implement deployment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo 'verify.0xcert.org' > CNAME
 git init
 git add -A
 git commit -m "Deploy website"
-git push -f git@github.com:0xcert/verify-ui.git master:gh-pages
+git push -f git@github.com:0xcert/verifier-ui.git master:gh-pages
 cd -
 echo ''
 echo '-----------------------'
