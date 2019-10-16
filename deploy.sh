8291b2f4e5a69be0756be0deea1f833602eef15e
davidlicen <david@artnetik.si>
2019-10-16 15:42:57 +0200

    Update deloyment

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,15 @@ npm install
 npm run generate
 cd dist
 echo 'verify.0xcert.org' > CNAME
+cat 
+echo '# 0xcert Verifier public website
+
+This upublic generated website of https://github.com/0xcert/verifier-ui/' > README.md
 git init
 git add -A
 git commit -m "Deploy website"
 git push -f git@github.com:0xcert/verifier-ui.git master:gh-pages
 cd -
-echo ''
 echo '-----------------------'
 echo '  Deployment complete. '
 echo '-----------------------'
